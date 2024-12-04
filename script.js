cd0cea2a92a2a5e57509276a20a0bbee232bd6cf
Hyning Gan <hyninggan@gmail.com>
2024-12-04 08:54:28 -0500

    description

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ const labs = [
     {
         title: "Lab 1. Perception Bias",
         subtitle: "Compositional Biases in Visual Perception",
-        description: "Compositional  in Visual Perception",
+        description: "Based on Palmer and Langlois (2017), this experiment investigates how we perceive and form preferences for different visual compositions in space.",
         url: "https://hihyning.github.io/Lab-1-Psych-of-Aesthetics/"
     },
     {
         title: "Lab 2. Colour Preferences",
         subtitle: "Individual Colour Preferences",
-        description: "Individual Colour Preferences",
+        description: "Palmer & Schloss (2010) investigated the 'ecological valence theory of human colour preferences,' this experiment explores how we naturally connect with and develop feelings about different colours.",
         url: "https://hihyning.github.io/Lab-2-Psych-of-Aesthetics/"
     },
     {
         title: "Lab 3. Celebrity Morphs",
         subtitle: "Face Perception & Recognition",
-        description: "Face Perception & Recognition",
+        description: "Drawing on Halberstadt et al (2013) on face perception, this experiment investigates how familiarity influences our aesthetic judgments.",
         url: "https://hihyning.github.io/Lab-3-Psych-of-Aesthetics/"
     },
     {
